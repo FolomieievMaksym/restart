2e6e09ba8949e08a2e6d0d94ebe803cbe1d39f4d
FolomieievMaksym <79655335+FolomieievMaksym@users.noreply.github.com>
2022-07-04 17:19:50 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 "use strict";
 const burger = document.querySelector('.burger'),
 	menu = document.querySelector('.menu'),
-	body = document.querySelector('body');
+	body = document.querySelector('body'),
+	popup = document.querySelector('.popup'),
+	popupBody = document.querySelector('.popup__body'),
+	formButton = document.querySelector('.first-form__button');
 
 window.addEventListener('load', () => {
-	body.addEventListener('click', burgerToggle)
-	function burgerToggle(e) {
+
+
+
+	body.addEventListener('click', toggle)
+	function toggle(e) {
 		// alert("Click") // Для проверки вызова функции кликом
 		if (e.target.closest('.burger')) {
 			burger.classList.toggle('active');
@@ -16,5 +22,17 @@ window.addEventListener('load', () => {
 			menu.classList.remove('active');
 			body.classList.remove('lock')
 		}
+		if (e.target.closest('.first-form__button')) {
+			e.preventDefault();
+			popup.classList.add('active');
+			body.classList.add('lock');
+			popup.addEventListener('click', (e) => {
+				if (!e.target.closest('.popup__body') || e.target.closest('.popup__close')) {
+					popup.classList.remove('active');
+					body.classList.remove('lock');
+				}
+			})
+		}
+
 	}
 })
